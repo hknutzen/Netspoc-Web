f900be02d354268ee86479e762fc5cd6d4fc3ef5
Daniel Brunkhorst <daniel.brunkhorst@dataport.de>
2010-12-27 13:29:03 +0000

    Removed function proxy4path from here, because it was factored out to separate file common.js that is now loaded in start.html .

--- htdocs/resources/js/NetspocManager/PolicyManager.js
+++ htdocs/resources/js/NetspocManager/PolicyManager.js
@@ -1,14 +1,6 @@
 
 Ext.ns("NetspocManager");
 
-function proxy4path ( path ) {
-    return new Ext.data.HttpProxy(
-	{
-	    url : '/netspoc/' + path
-	}
-    );
-}
-
 /**
  * @class NetspocManager.PolicyManager
  * @extends Ext.Panel
