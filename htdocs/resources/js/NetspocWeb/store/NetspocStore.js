2ea2a5543863e216baa3d64ef46f7b6e090e28d1
Heinz Knutzen <heinz.knutzen@dataport.de>
2011-02-02 08:24:00 +0000

    Initial revision

---
+++ htdocs/resources/js/NetspocWeb/store/NetspocStore.js
@@ -0,0 +1,36 @@
+Ext.ns('NetspocWeb.store');
+
+NetspocWeb.store.Netspoc = Ext.extend(
+    Ext.data.JsonStore,
+    {
+	constructor : function(config) {
+	    Ext.apply(config,
+		      {
+    			  totalProperty : 'totalCount',
+			  successProperty : 'success',
+			  root          : 'records',
+			  remoteSort    : false,
+			  autoload      : true,
+			  url           : '/netspoc/' + config.proxyurl
+		      });
+	    NetspocWeb.store.Netspoc.superclass.constructor.call(this, config);
+	    this.on('exception', this.onJsonException, this, { scope : this });
+	},
+
+	onJsonException :
+	function(proxy, type, action, options, response, arg ) {
+	    var msg;
+	    // Use other class, because method onAfterLogout of that class is 
+	    // called by showJsonError
+	    if (type == 'response') {
+		NetspocManager.workspace.onAfterAjaxReq(this, true, response);
+	    }
+	    else {
+		NetspocManager.workspace.showJsonError(response.msg);
+	    }
+	    
+	}
+    }
+);
+
+Ext.reg( 'netspocstore', NetspocWeb.store.Netspoc);
