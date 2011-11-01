b07f4fef74940c05e276a94f6160bf04eb435e4b
Daniel Brunkhorst <daniel.brunkhorst@dataport.de>
2011-11-01 10:11:20 +0000

    Refactored code so that the code constructing the search window is now in SearchFormWindow.js. Changed search_window to be a local variable.

--- htdocs/resources/js/NetspocManager/PolicyManager.js
+++ htdocs/resources/js/NetspocManager/PolicyManager.js
@@ -11,6 +11,8 @@ Ext.ns("NetspocManager");
  * @xtype policymanager
  **/
 
+var search_window;
+
 NetspocManager.PolicyManager = Ext.extend(
     Ext.Container,
     {
@@ -105,18 +107,17 @@ NetspocManager.PolicyManager = Ext.extend(
 	},
 	
 	onButtonClick :  function(button, event) {
-	    var sfw        = Ext.getCmp( 'searchFormWindowId' );
 	    var plv        = this.getComponent('policyListId');
 	    var relation   = button.relation;
 	    var params     = button.search_params;
 	    var keep_front = false;
 	    
 	    if ( params ) {
-		    keep_front = params.keep_front;
+		keep_front = params.keep_front;
 	    }
 	    
-	    if ( sfw && !keep_front ) {
-		sfw.hide();
+	    if ( search_window && !keep_front ) {
+		search_window.hide();
 	    }
 	    if ( relation ) {
 		params = { relation : relation };
@@ -126,224 +127,12 @@ NetspocManager.PolicyManager = Ext.extend(
 	},
 
 	displaySearchWindow :  function( button, event ) {
-
-	    var searchtext = {
-		xtype      : 'textfield',
-		id         : 'search_string',
-		width      : 330,
-		emptyText  : 'Zeichenkette oder IP eingeben ... ',
-		fieldLabel : 'Suchbegriff',
-		allowBlank : false,
-		minLength  : 2,
-		listeners: {
-                    specialkey: function( field, e ){
-			// Handle ENTER key press.
-			if ( e.getKey() == e.ENTER ) {
-			    var sb   = Ext.getCmp( 'buttonId' );
-			    var v    = Ext.getCmp( 'viewportId' );
-			    var pm   = v.findByType("policymanager");
-                            var form = field.ownerCt.getForm();
-			    sb.search_params = form.getValues();
-			    pm[0].onButtonClick( sb );
-			}
-                    }
-		}
-	    };
-	    
-	    var checkbox_group1 = {
-		xtype      : 'checkboxgroup',
-		fieldLabel : 'Suche in',
-		anchor     : '100%',
-		columns    : 2,
-		flex       : 1,
-		defaults   : {
-		    checked    : true
-		},
-		items      : [
-		    {
-			boxLabel   : 'Eigene Dienste',
-			name       : 'search_own'
-		    },
-		    {
-			id         : 'cb-rule-id',
-			boxLabel   : 'Regeln der Dienste',
-			name       : 'search_in_rules'
-		    },
-		    {
-			boxLabel   : 'Genutzte Dienste',
-			name       : 'search_used'
-		    },
-		    {
-			id         : 'cb-user-id',
-			boxLabel   : 'Nutzer der Dienste (User)',
-			name       : 'search_in_user'
-		    },
-		    {
-			boxLabel   : 'Nutzbare Dienste',
-			name       : 'search_visible'
-		    },
-		    {
-			id         : 'cb-desc-id',
-			boxLabel   : 'Dienstbeschreibungen',
-			name       : 'search_in_desc'
-		    },
-		    {
-			boxLabel   : 'Alle',
-			name       : 'search_in_all_services',
-			handler    : function( cb, checked ) {
-			    var cbg = cb.findParentByType( 'checkboxgroup' );
-			    if ( checked === true ) {
-				cbg.setValue( [ true, true, true ] );
-			    }
-			    else {
-				cbg.setValue( [ false, false, false ] );
-			    }
-			}
-		    },
-		    {
-			boxLabel   : 'Alle',
-			name       : 'search_in_all_details',
-			handler    : function( cb, checked ) {
-			    var cbg = cb.findParentByType( 'checkboxgroup' );
-			    if ( checked === true ) {
-				cbg.setValue(
-				    {
-					'cb-rule-id' : true,
-					'cb-user-id' : true,
-					'cb-desc-id' : true
-				    }
-				);
-			    }
-			    else {
-				cbg.setValue(
-				    {
-					'cb-rule-id' : false,
-					'cb-user-id' : false,
-					'cb-desc-id' : false
-				    }
-				);
-			    }
-			}
-		    }
-		]
-	    };
-	    
-	    var checkbox_group2 = {
-		xtype      : 'checkboxgroup',
-		anchor     : '100%',
-		columns    : 1,
-		flex       : 2,
-		defaults   : {
-		    checked    : true
-		},
-		items      : [
-		    {
-			boxLabel   : 'Groß-/Kleinschreibung beachten',
-			name       : 'search_case_sensitive'
-		    },
-		    {
-			boxLabel   : 'Such-Fenster im Vordergrund halten',
-			name       : 'keep_front',
-			checked    : false
-		    }
-		]
-	    };
-
-	    var checkbox_container = {
-		xtype  : 'container',
-		layout : 'form',
-		height : 120,
-		items : [
-		    checkbox_group1,
-		    { height : 10 },
-		    checkbox_group2
-
-		]
-	    };
-	    
-	    var radio_group = {
-		xtype      : 'radiogroup',
-		anchor     : '100%',
-		//fieldLabel : 'IP oder String',
-		items      : [
-		    {
-			boxLabel   : 'Zeichenkette',
-			name       : 'search_ip_or_string'
-		    },
-		    {
-			boxLabel   : 'IP-Adresse',
-			name       : 'search_ip_or_string'
-		    }
-		]
-	    };
-
-	    var myFormPanel = new Ext.form.FormPanel(
-		{
-		    id           : 'myFormPanelId',
-		    width        : 350,
-		    height       : 250,
-		    frame        : true,
-		    bodyStyle    : 'padding: 6px',
-		    labelWidth   : 70,
-		    buttonAlign  : 'center',
-		    layoutConfig : {
-//			align : 'stretch'
-		    },
-		    items        : [
-			{ height : 10 },
-			searchtext,
-			{ height : 10 },
-//			radio_group,
-			checkbox_container
-		    ]
-		}
-	    );
-
-	    var form = myFormPanel.getForm();
-	    var search_button =
-		myFormPanel.addButton( 
-		    {   // button config
-			id   : 'buttonId',
-			text : 'Suche starten'
-		    },
-		    function( button, event ) {  // button click handler
-			if ( form.isValid() ) {
-			    button.search_params = form.getValues();
-			    var v   = Ext.getCmp( 'viewportId' );
-			    var pm  = v.findByType("policymanager");
-			    pm[0].onButtonClick( button );
-			} else {
-			    var m = 'Bitte Eingaben in rot markierten ' 
-				+ 'Feldern korrigieren.';
-			    Ext.MessageBox.alert( 'Fehlerhafte Eingabe!', m );
-			}
-		    },
-		    myFormPanel    // scope in which the button handler function is executed
-		);
-	    
-	    var sfw = Ext.getCmp( 'searchFormWindowId' );
-	    if ( sfw ) {
-		sfw.show();
-		return;
+	    if ( search_window ) {
+		search_window.show();
 	    }
 	    else {
-		new Ext.Window(
- 		    {
-			id        : 'searchFormWindowId',
-			title     : 'IP-Adresse oder Zeichenkette suchen',
- 			width     : 450, 
- 			height    : 290,
- 			layout    : 'fit',
-			resizable : false,
- 			items     : [
-			    myFormPanel
- 			],
-			focus     : function() {  // Focus search text field.
-			    var txt_fields = this.findByType( 'textfield' );
-			    txt_fields[0].focus( false, true );
- 			}
-		    }
- 		).show();
+		search_window = new NetspocWeb.window.SearchFormWindow();
+ 		search_window.show();
 	    }
 	},
 
