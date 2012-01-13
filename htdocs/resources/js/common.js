3c005a4e92930b7103a81e09cb4c944f24a90b0d
Daniel Brunkhorst <daniel.brunkhorst@dataport.de>
2012-01-13 17:40:45 +0100

    Added functions "numeric2ip" and "ip2numeric" that convert IPs from numeric format to dot-notation and vice versa.

--- htdocs/resources/js/common.js
+++ htdocs/resources/js/common.js
@@ -20,3 +20,17 @@ function captureEvents(observable) {
         this
     );		
 }
+
+function ip2numeric( dot ) {
+    var d = dot.split('.');
+    return ((((((+d[0])*256)+(+d[1]))*256)+(+d[2]))*256)+(+d[3]);
+}
+
+function numerip2ip( num ) {
+    var d = num%256;
+    for (var i = 3; i > 0; i--) {
+	num = Math.floor(num/256);
+	d = num%256 + '.' + d;
+    }
+    return d;
+}
