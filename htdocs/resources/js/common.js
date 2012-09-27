c1c6f8f041e9d7e582f40bf9e805423fb18c6fa9
Heinz Knutzen <heinz.knutzen@dataport.de>
2012-09-27 14:52:44 +0200

    Removed unused function proxy4path

--- htdocs/resources/js/common.js
+++ htdocs/resources/js/common.js
@@ -1,11 +1,4 @@
 
-function proxy4path ( path ) {
-    return new Ext.data.HttpProxy(
-	{
-	    url : '/netspoc/' + path
-	}
-    );
-}
 
 function trim( str ) {
     return str.replace(/^\s+|\s+$/g,"");
