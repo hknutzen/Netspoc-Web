09cf24f6a905ddf43af5c198294fb24704a62964
Daniel Brunkhorst <daniel.brunkhorst@dataport.de>
2013-06-28 13:26:21 +0200

    Changed Network grid to inherit from Abstract grid
    Now that the bug was found leading to "checkHd.el is undefined"
    the inheritance could be fixed and code duplication is avoided!

--- app/view/panel/grid/Networks.js
+++ app/view/panel/grid/Networks.js
@@ -2,18 +2,13 @@
 Ext.define(
     'PolicyWeb.view.panel.grid.Networks',
     {
-        extend      : 'Ext.grid.Panel',
+        extend      : 'PolicyWeb.view.panel.grid.Abstract',
         alias       : 'widget.networklist',
         controllers : [ 'Network' ],
         store       : 'Networks',
         forceFit    : true,
         flex        : 2,
         border      : false,
-        selModel    : {
-            selType     : 'checkboxmodel',
-            mode        : 'MULTI',
-            headerWidth : 12
-        },
         viewConfig  : {
             loadMask : false
         },
@@ -22,15 +17,12 @@ Ext.define(
             { text : 'Name',                  dataIndex : 'name'  },
             { text : 'Verantwortungsbereich', dataIndex : 'owner' }
         ],
-        printview : function() {
-            Ext.ux.grid.Printer.print( this );
-        },
-        
-        select0 : function() {
-            if ( this.getStore().getCount() > 0 ) {
-                var selmodel = this.getSelectionModel();
-                selmodel.select(0);
-            }
+        buildSelModel : function() {
+            return {
+                selType     : 'checkboxmodel',
+                mode        : 'MULTI',
+                headerWidth : 12
+            };
         }
     }
 );
