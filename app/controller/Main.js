3726a2d6d478c2a9fbaec767e9da2ca6b44a4236
Daniel Brunkhorst <daniel.brunkhorst@dataport.de>
2014-09-30 10:54:15 +0200

    Fix: keep type of services on owner change.
    On owner change, the type of services was always reset
    to "user", which was not the desired behaviour.
    
    Even worse: the type was changed, but the button that is
    used to manually change the type of services displayed was not changed,
    so it could happen that used services were displayed but the button
    for the display of own services was toggled on in the GUI.

--- app/controller/Main.js
+++ app/controller/Main.js
@@ -208,8 +208,10 @@ Ext.define(
         onPolicyLoaded : function( records, operation, success ) {
             var owner_ob = operation.owner;
             if (success && records.length) {
-                // Don't fire change event.
-                appstate.changeHistory(records[0], true);
+                appstate.changeHistory(
+                    records[0],
+                    true   // Don't fire change event.
+                );
             }
             appstate.changeOwner(owner_ob.name, owner_ob.alias);
             operation.store.destroy();
@@ -223,8 +225,9 @@ Ext.define(
             this.getHistoryStore().load();
             var store = this.getServiceStore();
             var proxy = store.getProxy();
-            // 'user' is default relation.
-            proxy.extraParams.relation = 'user';
+            var service_controller = this.getController( 'Service' );
+            var relation = service_controller.getCurrentRelation();
+            proxy.extraParams.relation = relation;
             store.load();
         },
             
