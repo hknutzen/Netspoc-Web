630172170dc3b8cc949634d2ffef12953b37dff7
Daniel Brunkhorst <daniel.brunkhorst@dataport.de>
2019-03-12 11:18:42 +0100

    Change store from type Netspoc to NetspocState
    This store needs history and owner params, so it needs state
    and therefore to be a NetspocState store.
    It must have been changed to Netspoc errorneously on a previous
    commit.

--- app/store/DiffSetMail.js
+++ app/store/DiffSetMail.js
@@ -3,7 +3,7 @@
 Ext.define(
     'PolicyWeb.store.DiffSetMail',
     {
-        extend      : 'PolicyWeb.store.Netspoc',
+        extend      : 'PolicyWeb.store.NetspocState',
         model       : 'PolicyWeb.model.DiffSetMail',
         autoLoad    : false,
         autoDestroy : true
