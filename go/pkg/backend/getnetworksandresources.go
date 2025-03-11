50d98cf2117008c85a8635922658cf0820ea78b4
Daniel Brunkhorst <daniel.brunkhorst@dataport.de>
2025-03-11 11:03:45 +0100

    URL /get_networks_and_resources nach go migriert

---
+++ go/pkg/backend/getnetworksandresources.go
@@ -0,0 +1,105 @@
+package backend
+
+import (
+	"maps"
+	"net/http"
+	"slices"
+)
+
+func (s *state) getNetworks(r *http.Request) []jsonMap {
+	owner := r.FormValue("active_owner")
+	chosen := r.FormValue("chosen_networks")
+	history := r.FormValue("history")
+	assets := s.loadAssets(history, owner)
+	networkNames := assets.networkList
+	if chosen != "" {
+		networkNames = untaintNetworks(chosen, assets)
+	}
+	return getNatObjList(s, networkNames, owner, history)
+}
+
+func (s *state) getNetworkResourcesForNetworks(r *http.Request, selected string) []jsonMap {
+	var data []jsonMap
+	if selected != "" {
+		owner := r.FormValue("active_owner")
+		history := r.FormValue("history")
+		assets := s.loadAssets(history, owner)
+		networkNames := untaintNetworks(selected, assets)
+		natSet := s.loadNATSet(history, owner)
+		objects := s.loadObjects(history)
+		for _, networkName := range networkNames {
+			childNames := assets.net2childs[networkName]
+			for _, childName := range childNames {
+				obj := getObject(objects, childName)
+				entry := jsonMap{
+					"name":       networkName,
+					"childIP":    name2IP(s, history, childName, natSet),
+					"childName":  childName,
+					"childOwner": obj.Owner,
+				}
+				data = append(data, entry)
+			}
+		}
+	}
+	return data
+}
+
+func (s *state) getNetworkResources(w http.ResponseWriter, r *http.Request) {
+	if !loggedIn(r) {
+		w.WriteHeader(http.StatusUnauthorized)
+		return
+	}
+	selected := r.FormValue("selected_networks")
+	result := s.getNetworkResourcesForNetworks(r, selected)
+	writeRecords(w, result)
+}
+
+func (s *state) getNetworksAndResources(w http.ResponseWriter, r *http.Request) {
+	var result []jsonMap
+	if !loggedIn(r) {
+		w.WriteHeader(http.StatusUnauthorized)
+		return
+	}
+	networks := s.getNetworks(r)
+	netsAsCSV := ""
+	for _, network := range networks {
+		netsAsCSV += network["name"].(string) + ","
+	}
+	type Network struct {
+		Name     string    `json:"name"`
+		IP       string    `json:"ip"`
+		Owner    string    `json:"owner"`
+		Children []jsonMap `json:"children"`
+	}
+	data := s.getNetworkResourcesForNetworks(r, netsAsCSV)
+	net2data := make(map[string]Network)
+	for _, network := range networks {
+		net2data[network["name"].(string)] = Network{
+			Name:     network["name"].(string),
+			IP:       network["ip"].(string),
+			Owner:    network["owner"].(string),
+			Children: []jsonMap{},
+		}
+	}
+	for _, resource := range data {
+		child := jsonMap{
+			"ip":    resource["childIP"],
+			"name":  resource["childName"],
+			"owner": resource["childOwner"],
+		}
+		name := resource["name"].(string)
+		network := net2data[name]
+		network.Children = append(network.Children, child)
+		net2data[name] = network
+	}
+	keys := slices.Sorted(maps.Keys(net2data))
+	for _, netName := range keys {
+		result = append(result, jsonMap{
+			"name":     netName,
+			"ip":       net2data[netName].IP,
+			"owner":    net2data[netName].Owner,
+			"children": net2data[netName].Children,
+		})
+	}
+	writeRecords(w, result)
+}
