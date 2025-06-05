693e04828c9beeaddf0332ad7f72d9a34e650751
Daniel Brunkhorst <daniel.brunkhorst@dataport.de>
2025-06-05 17:12:02 +0200

    Implement own RecoveryHandler instead of gorilla's
    Gracefully rcover from panics, for example when a request with
    an unknown owner is made and the loading of resources results
    in a panic.

--- go/pkg/backend/main.go
+++ go/pkg/backend/main.go
@@ -7,8 +7,6 @@ import (
 	"net/http/httputil"
 	"net/url"
 	"os"
-
-	"github.com/gorilla/handlers"
 )
 
 type state struct {
@@ -70,10 +68,23 @@ func getMux() *http.ServeMux {
 }
 
 func MainHandler() http.Handler {
-	return handlers.RecoveryHandler( /*handlers.PrintRecoveryStack(true)*/ )(getMux())
+	return RecoveryHandler(getMux())
+}
+
+func RecoveryHandler(h http.Handler) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		defer func() {
+			if err := recover(); err != nil {
+				msg := fmt.Errorf("%s", err)
+				writeError(w, msg.Error(), http.StatusInternalServerError)
+			}
+		}()
+		h.ServeHTTP(w, r)
+	})
 }
 
 func writeError(w http.ResponseWriter, errorMsg string, httpStatus int) {
+	w.Header().Set("Connection", "close")
 	w.Header().Set("Content-Type", "text/x-json")
 	w.WriteHeader(httpStatus)
 	// Flusher is only needed temporarily.
