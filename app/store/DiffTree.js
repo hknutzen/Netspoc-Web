f91276089d90ecc4b0d773ede502827411d53ec8
Daniel Brunkhorst <daniel.brunkhorst@dataport.de>
2025-06-17 13:28:30 +0200

    Fix missing API endpoints for double backend

--- app/store/DiffTree.js
+++ app/store/DiffTree.js
@@ -18,34 +18,34 @@ along with this program. If not, see <http://www.gnu.org/licenses/>.
 
 Ext.define(
     'PolicyWeb.store.DiffTree', {
-        extend      : 'Ext.data.TreeStore',
-        alias       : 'store.difftreestore',
-        autoLoad    : false,
-        // Send  value of 'id' of root node as parameter 'version'.
-        nodeParam   : 'version',
-        translation : {
-            'service_lists owner' : 'Liste eigener Dienste',
-            'service_lists user'  : 'Liste genutzter Dienste',
-            'objects'             : 'Objekte',
-            'services'            : 'Dienste',
-            'users'               : 'Liste der Benutzer (User)'
-        },
-        rename : function (child) {
-            var txt = child.raw.text;
-            var out = this.translation[txt];
-            if ( out ) {
-                child.set( 'text', out );
-            }
-        },
-        root            : {
-            text : 'Bitte Stand auswählen in "Vergleiche mit".',
-            id   : 'none'
-        },
-        proxy  : {
-            type    : 'ajax',
-            noCache : false,
-            url     : 'backend/get_diff'
+    extend: 'Ext.data.TreeStore',
+    alias: 'store.difftreestore',
+    autoLoad: false,
+    // Send  value of 'id' of root node as parameter 'version'.
+    nodeParam: 'version',
+    translation: {
+        'service_lists owner': 'Liste eigener Dienste',
+        'service_lists user': 'Liste genutzter Dienste',
+        'objects': 'Objekte',
+        'services': 'Dienste',
+        'users': 'Liste der Benutzer (User)'
+    },
+    rename: function (child) {
+        var txt = child.raw.text;
+        var out = this.translation[txt];
+        if (out) {
+            child.set('text', out);
         }
+    },
+    root: {
+        text: 'Bitte Stand auswählen in "Vergleiche mit".',
+        id: 'none'
+    },
+    proxy: {
+        type: 'ajax',
+        noCache: false,
+        url: 'backend6/get_diff'
     }
+}
 );
 
