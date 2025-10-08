7d8c8256c6b7652cac1c458eb16881a1c7a5bf3a
Daniel Brunkhorst <daniel.brunkhorst@dataport.de>
2025-10-08 16:14:05 +0200

    Handler for /get_owners, session from go, 2 helper funcs
    Get session from go code instead of Perl code.
    
    Implement handler for URL /get_owners.
    
    Implement two helper funcs, "canAccessOwner" and "validateOwner".

--- go/pkg/backend/getowner.go
+++ go/pkg/backend/getowner.go
@@ -6,13 +6,37 @@ import (
 	"strings"
 )
 
+func (s *state) getOwners(w http.ResponseWriter, r *http.Request) {
+	// Return all owners that the logged-in user is authorized to access.
+	// This is used to populate the owner selection dropdown.
+	authorizedOwners := s.findAuthorizedOwners(r)
+	if len(authorizedOwners) == 0 {
+		writeRecords(w, []jsonMap{})
+		return
+	}
+	// Return as a list of {"name": owner}.
+	var owners []map[string]string
+	for _, owner := range authorizedOwners {
+		owners = append(owners, map[string]string{"name": owner})
+	}
+	writeRecords(w, owners)
+}
+
 func (s *state) getOwner(w http.ResponseWriter, r *http.Request) {
-	ow := getSessionFromPerl(r).Owner
+	session := GetGoSession(r)
+	owner := session.Get("owner")
+	ow := ""
+	if owner != nil {
+		ow = owner.(string)
+		if s.canAccessOwner(r, ow) {
+			writeRecords(w, []jsonMap{{"name": ow}})
+			return
+		}
+	}
 	l := s.findAuthorizedOwners(r)
 	// Selected owner was stored before.
 	if ow != "" && slices.Contains(l, ow) {
 		writeRecords(w, []jsonMap{{"name": ow}})
-		return
 	}
 	// Automatically select owner with most number of own services.
 	best := ""
@@ -27,7 +51,7 @@ func (s *state) getOwner(w http.ResponseWriter, r *http.Request) {
 		}
 	}
 	if best != "" {
-		setOwner(r, best)
+		session.Put("owner", best)
 		writeRecords(w, []jsonMap{{"name": best}})
 		return
 	}
@@ -36,11 +60,38 @@ func (s *state) getOwner(w http.ResponseWriter, r *http.Request) {
 
 func (s *state) findAuthorizedOwners(r *http.Request) []string {
 	m := s.loadEmail2Owners()
-	email := getSessionFromPerl(r).Email
+	email := GetGoSession(r).Get("email").(string)
 	_, dom, _ := strings.Cut(email, "@")
 	wildcard := "[all]@" + dom
 	result := slices.Concat(m[wildcard], m[email])
 	slices.Sort(result)
 	result = slices.Compact(result)
 	return result
+}
+
+// Validate active owner.
+// Email could be removed from any owner role at any time in netspoc data.
+func (s *state) validateOwner(r *http.Request, ownerNeeded bool) {
+	activeOwner := r.URL.Query().Get("active_owner")
+	if activeOwner != "" {
+		if !ownerNeeded {
+			abort("Must not send parameter 'active_owner'")
+		}
+		if !s.canAccessOwner(r, activeOwner) {
+			abort("Not authorized to access owner '" + activeOwner + "'")
+		}
+	} else {
+		if ownerNeeded {
+			abort("Missing parameter 'active_owner'")
+		}
+	}
+}
+
+func (s *state) canAccessOwner(r *http.Request, owner string) bool {
+	for _, authorizedOwner := range s.findAuthorizedOwners(r) {
+		if owner == authorizedOwner {
+			return true
+		}
+	}
+	return false
 }
